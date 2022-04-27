f049e04ca070821b1f4ee484d2ebb03399e54f73
PSUMIT1995 <psumit191.sp@gmail.com>
2022-04-27 22:37:40 +0530

    UC4 : To Delete a Contact from AddressBook

--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -9,14 +9,15 @@ namespace AddressBook
     internal class Program
     {
 
+
         //Main  Method / program Entry Point
         static void Main(string[] args)
         {
+            //Showing Welcome Message On console.
+            Console.WriteLine("Welcome to Address Book Progam \n");
             AddressBook createAddressBook = new AddressBook();
             createAddressBook.ReadInput();
             Console.ReadLine();
-
-
 
         }
     }
